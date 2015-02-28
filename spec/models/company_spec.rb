32e75409a4066f98aa66d5ae2fb6581001599422
Kyra Weber <kyra.m.weber@gmail.com>
2015-02-28 12:36:42 -0700

    Validates name for company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.create(name: "my company") }
+
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is not valid without a name" do
+    company.name = nil
+    expect(company).not_to be_valid
+  end
 end
